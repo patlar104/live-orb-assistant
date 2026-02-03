d68f2e9ba95a0189f2ca7906854372d19d52d29f
patlar104 <patrick.lar1214@gmail.com>
2026-02-03 10:09:35 -0500

    Add ESLint config and lint script

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['dist', 'node_modules'],
+  rules: {
+    'no-console': 'off'
+  }
+};
